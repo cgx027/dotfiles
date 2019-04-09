151156593917aa92623a4ebe50a4636e63025f4c
Ted Chen <tedc@vmware.com>
2019-04-09 18:59:54 +0800

    add code for macos

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ dir=$PWD
 bakdir=~/backup/dotfiles
 mkdir -p $bakdir
 
-for file in $(find ./files -type f -printf "%f ");do
+for file in $(find ./files -type f | xargs -n1 basename);do
 mv ~/.$file $bakdir/.$file
 ln -s $dir/files/$file ~/.$file
 done
